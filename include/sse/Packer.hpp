c57fbee42043b68a5632ced07278703829afeeed
Karl Nilsson <karl.robert.nilsson@gmail.com>
2020-01-24 20:08:02 -0500

    add documentation to bin packer

--- include/sse/Packer.hpp
+++ include/sse/Packer.hpp
@@ -16,73 +16,177 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * @file Packer.hpp
+ * @brief Packs a objects into a rectangular bin
+ *
+ * This contains the prototypes for the Packer class
+ *
+ * @author Karl Nilsson
+ * @bug fixed/static offset dimension between objects
+ */
 
 #pragma once
 
 #include <algorithm>
 #include <exception>
 #include <vector>
+#include <memory>
 
 #include <sse/Object.hpp>
 
 #include <spdlog/spdlog.h>
 
-
 /**
  * TODO:
- * allow for configurable OFFSET space between footprint rectangles
+ * allow for configurable/dynamic OFFSET space between footprint rectangles
  * (i.e. expand all rectangles individually, based on brim, if applicable.
  * keep in mind, brim may not expand footprint, i.e. brim for a sphere)
  */
-// buffer space 10mm each direction
+/// buffer space 10mm each direction
 #define OFFSET 10
 
 namespace sse {
 
 /**
+ * @class Node
  * @brief Binary tree Node
- * This class describes a binary tree Node that corresponds to a rectangle in the cartesian
- * plane. For the purposes of this class, width is a dimension in the X axis, and length in the Y axis.
+ *
+ * This class describes a binary tree Node that corresponds to a rectangle in
+ * the cartesian plane. The tree is used to pack objects into a rectangular bin.
+ * For the purposes of this class, width is a dimension in the X axis, and
+ * length in the Y axis.
  */
 class Node {
+  //! shorthand for shared_ptr
   using node_ptr = std::shared_ptr<Node>;
+
 public:
-  double x, y, width, length;
+  //! X position
+  const double x;
+  //! Y position
+  const double y;
+  //! node width
+  const double width;
+  //! node length
+  const double length;
+
+  /**
+   * @brief Node constructor
+   * @param y X position
+   * @param y Y position
+   * @param w Width, X axis
+   * @param l Length, Y axis
+   */
   Node(double x, double y, double w, double l);
-  // check to see if a rectangle will fit in this node
-  bool fits(std::shared_ptr<Object> o) {
+
+  /**
+   * @brief Check to see if object will fit in this node
+   * @param o Target object
+   * @return Whether object fits in node
+   */
+  inline bool fits(std::shared_ptr<Object> o) {
     return (o->length() + OFFSET < length) && (o->width() + OFFSET < width);
   }
-  // check to see if there's an object in this node
-  bool full() { return bool(object); }
-  // check to see if this node is a leaf node or not
-  bool leaf() { return bool(up);}
-  Node *search(std::shared_ptr<Object> o);
+
+  /**
+   * @brief Does this node contain an object?
+   * @return Whether node contains an object
+   */
+  inline bool full() { return object != nullptr; }
+
+  /**
+   * @brief is this node a leaf?
+   * @return Whether node is a leaf
+   */
+  inline bool leaf() { return up != nullptr; }
+
+  /**
+   * @brief Search the tree for a suitable node to hold an object
+   * @param o The object to insert
+   * @return pointer to a suitable node, nullptr otherwise
+   */
+  Node* search(std::shared_ptr<Object> o);
+
+  /**
+   * @brief Add object to node, then split remaining space into 2 child nodes
+   * @param o Object to be inserted
+   */
   void add_object(std::shared_ptr<Object> o);
+
+  /**
+   * @brief Translate object, then recurse to children
+   * @param offset_x X offset of bin with respect to buildplate origin
+   * @param offset_y Y offset of bin with respect to buildplate origin
+   */
   void translate(double offset_x, double offset_y);
+
+  /**
+   * @brief Add child nodes
+   * @param up Up child
+   * @param right Right child
+   */
   void add_children(node_ptr up, node_ptr right);
+
 private:
-  node_ptr up, right;
+  //! up child node
+  node_ptr up;
+  //! right child node
+  node_ptr right;
+  //! object contained in this node
   std::shared_ptr<Object> object;
 };
 
 /**
- * @brief Pack objects into the smallest rectangular bin, based on their XY bounding
- * box. Construct a binary tree of Nodes, growing the bin and tree to fit
- * objects. A few heuristics are employed to minimize wasted space, ensuring the resulting bin has close to equal dimensions (i.e. roughly a
- * square).
+ * @class Packer
+ * @brief Pack objects into a rectangular bin, based on their XY bounding box.
+ *
+ * Construct a binary tree of Nodes, growing the bin and tree to fit objects. A
+ * few heuristics are employed to minimize wasted space, ensuring the resulting
+ * bin has close to equal dimensions (i.e. roughly a square).
  *
  */
 class Packer {
 
 public:
+  /**
+   * @brief Packer constructor
+   * @param objects List of objects to pack
+   */
   Packer(std::vector<std::shared_ptr<Object>> objects);
+
+  /**
+   * @brief Calculate an optimized rectangular bin for the objects
+   * @return Dimensions of resulting bin
+   * @throws std::runtime Thrown if can't grow bin properly, or can't insert object after growing bin
+   */
   std::pair<double, double> pack();
+
+  /**
+   * @brief Translate objects to their new position
+   * @param offset_x X offset of bin with respect to buildplate origin
+   * @param offset_y Y offset of bin with respect to buildplate origin
+   */
   void translate(double offset_x, double offset_y);
+
 private:
+  /**
+   * @brief Grow the bin in the +Y direction
+   * @param w Width requested
+   * @param l Length requested
+   */
   void grow_up(double w, double l);
+
+  /**
+   * @brief Grow the bin in the +X direction
+   * @param w Width requested
+   * @param l Length requested
+   */
   void grow_right(double w, double l);
+
+  //! list of objects to pack
   std::vector<std::shared_ptr<Object>> objects;
+  //! root node of binary tree
   std::shared_ptr<Node> root;
 };
 
--- src/Packer.cpp
+++ src/Packer.cpp
@@ -16,38 +16,31 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * @file Packer.cpp
+ * @brief Packs objects into a rectangular bin
+ *
+ * Based on this excellent article https://codeincomplete.com/posts/bin-packing/
+ *
+ * @author Karl Nilsson
+ */
+
 #include <sse/Packer.hpp>
 
 namespace sse {
 
-/**
- * @brief Node::Node Constructor
- * @param x X position
- * @param y Y position
- * @param w width, x axis
- * @param l height, y axis
- */
-Node::Node(double x, double y, double w, double l) {
-  this->x = x;
-  this->y = y;
-  this->width = w;
-  this->length = l;
+Node::Node(double x, double y, double w, double l)
+    : x(x), y(y), width(w), length(l) {
   // ensure both children are null
-  this->up = nullptr;
-  this->right = nullptr;
+  up = nullptr;
+  right = nullptr;
   // initialize with no object
-  this->object = nullptr;
+  object = nullptr;
 }
 
-/**
- * @brief Node::insert Traverse the binary tree using depth-first-search,
- * searching for a suitable Node to hold the object
- * @param o The object to insert
- * @return pointer to a suitable node, nullptr otherwise
- */
-Node *Node::search(std::shared_ptr<Object> o) {
-  // leaf nodes are the only nodes valid for object insertion
-  // this also prevents inserting objects in the pseudo-nodes created when
+Node* Node::search(std::shared_ptr<Object> o) {
+  // leaf nodes are the only nodes valid for object insertion. this also
+  // prevents inserting objects in the extraneous root nodes created when
   // growing the bin
   if (leaf()) {
     // if node is full, or is too small, return
@@ -64,10 +57,6 @@ Node *Node::search(std::shared_ptr<Object> o) {
   }
 }
 
-/**
- * @brief Add object to a Node, then split remaining space into child nodes
- * @param o Object to be inserted
- */
 void Node::add_object(std::shared_ptr<Object> o) {
   object = o;
   // add child to the right of the inserted object
@@ -76,21 +65,11 @@ void Node::add_object(std::shared_ptr<Object> o) {
   up = std::make_shared<Node>(x, y + o->length(), width, length - o->length());
 }
 
-/**
- * @brief Add child nodes to a node
- * @param up Up child
- * @param right Right child
- */
 void Node::add_children(node_ptr up, node_ptr right) {
   this->up = up;
   this->right = right;
 }
 
-/**
- * @brief Visit every node in the tree (DFS), translating each
- * object to its new position
- *
- */
 void Node::translate(double offset_x, double offset_y) {
   // only translate if the node has an object
   if (full()) {
@@ -100,7 +79,6 @@ void Node::translate(double offset_x, double offset_y) {
     // perform translation
     object->translate(delta_x, delta_y, 0);
   }
-
   // recurse to children
   if (!leaf()) {
     up->translate(offset_x, offset_y);
@@ -108,19 +86,14 @@ void Node::translate(double offset_x, double offset_y) {
   }
 }
 
-/**
- * @brief Packer::Packer Constructor
- * @param objects List of Objects to pack
- */
-Packer::Packer(std::vector<std::shared_ptr<Object>> objects) {
-  this->objects = objects;
-
+Packer::Packer(std::vector<std::shared_ptr<Object>> objects)
+    : objects(objects) {
   // sort the objects, biggest to smallest, in terms of footprint
   // specifically, compare the largest dimension (X or Y) of each object
-  spdlog::debug("BinPack: sorting objects");
-  std::sort(objects.begin(), objects.end(), [](Object &lhs, Object &rhs) {
-    return std::max(lhs.length(), lhs.width()) >
-           std::max(rhs.length(), rhs.width());
+  spdlog::debug("BinPack: sorting object list");
+  std::sort(objects.begin(), objects.end(), [](std::shared_ptr<Object> lhs, std::shared_ptr<Object> rhs) {
+    return std::max(lhs->length(), lhs->width()) >
+           std::max(rhs->length(), rhs->width());
   });
   // create the root node. with dimensions equal to the first object
   // this is essential, so that we don't have to grow the bin in two dimensions
@@ -130,16 +103,13 @@ Packer::Packer(std::vector<std::shared_ptr<Object>> objects) {
                                 objects.front()->length() + OFFSET);
 }
 
-/**
- * @brief Packer::grow_up Grow the bin in the Y-direction
- * @param root Root node
- * @param w width requested
- * @param l length requested
- */
 void Packer::grow_up(double w, double l) {
+  // TODO: dynamic, object-specific offset
+  w += OFFSET;
+  l += OFFSET;
   // create new root node
   auto new_root = std::make_shared<Node>(0, 0, root->width, root->length + l);
-  // add child node in the new space
+  // create child node
   auto up_child = std::make_shared<Node>(0, root->length, w, l);
   // other child is the previous root node
   new_root->add_children(up_child, root);
@@ -147,28 +117,20 @@ void Packer::grow_up(double w, double l) {
   root = new_root;
 }
 
-/**
- * @brief Packer::grow_right Grow the bin in the X-direction
- * @param root Root node
- * @param w width requested
- * @param l length requested
- */
 void Packer::grow_right(double w, double l) {
+  // TODO: dynamic, object-specific offset
+  w += OFFSET;
+  l += OFFSET;
   // create new root node
   auto new_root = std::make_shared<Node>(0, 0, root->width + w, root->length);
-  // create a new child node,
+  // create child node
   auto right_child = std::make_shared<Node>(root->width, 0, w, l);
+  // other child is the previous root node
   new_root->add_children(root, right_child);
   // finally, change pointer to the new root
   root = new_root;
 }
 
-/**
- * @brief Packer::pack Calculate an optimized rectangular bin for a list of
- * objects
- * @return Dimensions of resulting bin
- * @throws std::runtime
- */
 std::pair<double, double> Packer::pack() {
   spdlog::debug("BinPack: packing");
   // insert all objects into the tree
@@ -176,15 +138,13 @@ std::pair<double, double> Packer::pack() {
     spdlog::debug("BinPack: searching for suitable node");
     // attempt to find a suitable node for the object
     auto result = root->search(o);
-
-    // no node found, so grow nodes
+    // no node found, so grow bin
     if (!result) {
       spdlog::debug("BinPack: insufficient space; growing node");
       // determine which direction to grow
       auto can_grow_up = o->width() < root->width;
       auto should_grow_up =
           can_grow_up && (root->length >= (root->width + o->width()));
-
       auto can_grow_right = o->length() < root->length;
       auto should_grow_right =
           can_grow_right && (root->width >= (root->leaf() + o->length()));
@@ -199,13 +159,14 @@ std::pair<double, double> Packer::pack() {
       } else if (can_grow_up) {
         grow_up(o->width(), o->length());
       } else {
+        // if we can't determine which direction to grow, throw an error
         spdlog::error(
             "BinPack Error: Can't determine correct growth direction of bin");
         throw std::runtime_error(
             "BinPack: Can't determine correct growth direction of bin");
       }
 
-      // re-attempt search
+      // retry search
       result = root->search(o);
       // if search failed after growing, something is wrong, throw error
       if (!result) {
@@ -214,20 +175,19 @@ std::pair<double, double> Packer::pack() {
             "Bin Packing error: failed to fit object after growing node");
       }
     }
-    spdlog::debug("BinPack: adding object to tree: {0:d}x{1:d} @ {2:d},{3:d}",
+    // print object dimensions and location in bin
+    spdlog::debug("BinPack: adding object to tree: {.3f}x{.3f} @ ({.3f},{.3f})",
                   o->width(), o->length(), result->x, result->y);
     // add object to suitable node
     result->add_object(o);
   }
 
-  // return the dimensions of the bin, which are also the dimensions of the root
-  // node
+  // return the dimensions of the bin: (width, length)
   return std::make_pair(root->width, root->length);
 }
 
 void Packer::translate(double offset_x, double offset_y) {
   spdlog::debug("BinPack: translating objects to new location");
-  // translate each object to its destination
   root->translate(offset_x, offset_y);
 }
 
