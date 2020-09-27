3ee30acb03ac98223d298f014444b521c14bd1cb
Karl Nilsson <karl.robert.nilsson@gmail.com>
2020-09-27 16:23:48 -0400

    refactor unit tests for Packer

--- tests/test_binpack.cpp
+++ tests/test_binpack.cpp
@@ -9,26 +9,27 @@
 #include <memory>
 #include <iostream>
 
-TEST_CASE("Packer parameter sanitization") {
+
+TEST_CASE("Bin Packer") {
   // create empty vector
   auto objects = std::vector<std::shared_ptr<sse::Object>>();
   // ensure vector is empty
   REQUIRE(objects.size() == 0);
 
 
-  SUBCASE("testing empty binpacker") {
+  SUBCASE("Empty argument list") {
     // should throw an error
     CHECK_THROWS_AS(sse::Packer p{objects}, std::runtime_error);
   }
 
-  SUBCASE("testing binpacker with empty object") {
+  SUBCASE("Empty object") {
     // add object with zero volume
     auto a = TopoDS_Shape();
     objects.push_back(std::make_shared<sse::Object>(a));
     CHECK_THROWS_AS(auto p = sse::Packer(objects), std::runtime_error);
   }
 
-  SUBCASE("testing binpacker with infinite volume object") {
+  SUBCASE("Infinite volume object") {
     // XY plane
     auto face = BRepBuilderAPI_MakeFace(gp_Pln(gp::Origin(), gp::DZ()));
     // create a halfspace
@@ -40,49 +41,68 @@ TEST_CASE("Packer parameter sanitization") {
     CHECK_THROWS_AS(auto p = sse::Packer(objects), std::runtime_error);
   }
 
-  SUBCASE("testing binpacker with too many objects") {
+  SUBCASE("Too many objects") {
     // create excessive objects objects
-    BRepPrimAPI_MakeBox box{1,1,1};
-    box.Build();
+    BRepPrimAPI_MakeBox box{1, 1, 1};
     // MAXIMUM_OBJECTS is defined in Packer.hpp
     for (auto i = 0; i <= MAXIMUM_OBJECTS; ++i) {
-        auto a = box.Solid();
+        auto a = box.Shape();
         objects.push_back(std::make_shared<sse::Object>(a));
     }
     CHECK_THROWS_AS(auto p = sse::Packer(objects), std::runtime_error);
   }
-}
 
+  SUBCASE("Valid Tests") {
+    // create a simple box
+    BRepPrimAPI_MakeBox box_maker{10, 10, 10};
+    auto a = box_maker.Shape();
 
-TEST_CASE("Packer cubes test") {
-  // create empty vector
-  auto objects = std::vector<std::shared_ptr<sse::Object>>();
-  // ensure vector is empty
-  REQUIRE(objects.size() == 0);
-  // make a box with one corner at origin, with X,Y,Z dimensions of 10
-  auto box = BRepPrimAPI_MakeBox(10, 10, 10);
-  box.Build();
-  auto a = box.Shape();
+    SUBCASE("One Cube") {
+      objects.push_back(std::make_shared<sse::Object>(a));
+      REQUIRE(objects.size() == 1);
+      auto p = sse::Packer(objects);
+      auto [x,y] = p.pack();
+      CHECK_EQ(x, objects[0]->width());
+      CHECK_EQ(y, objects[0]->length());
+      p.arrange(0,0);
+      // calculate the correct move location
+      gp_Pnt corner{0,0,0};
+      // FIXME: currently broken
+      // CHECK(corner.IsEqual(objects[0]->get_bound_box().CornerMin(), 0.0001));
+    }
 
-  SUBCASE("List of squares") {
-    for (auto i = 1; i < 16; ++i) {
-      CAPTURE(i);
-      SUBCASE("") {
-      // SUBCASE(fmt::format("{i} squares", i).c_str()) {
-        // push all objects to vector
-        for (auto j = 0; j < i; ++j) {
-          objects.push_back(std::make_shared<sse::Object>(a));
+    SUBCASE("Double Pack") {
+      objects.push_back(std::make_shared<sse::Object>(a));
+      REQUIRE(objects.size() == 1);
+      auto p = sse::Packer(objects);
+      // attempt to pack twice, this should work
+      p.pack();
+      p.pack();
+    }
+
+    SUBCASE("List of cubes:") {
+      for (auto i = 1; i < 16; ++i) {
+        CAPTURE(i);
+        SUBCASE("") {
+          // push all objects to vector
+          for (auto j = 0; j < i; ++j) {
+            objects.push_back(std::make_shared<sse::Object>(a));
+          }
+          auto p = sse::Packer(objects);
+
+          // the bin dimensions of a list of cubes (X elements long) will always be
+          // ceil(sqrt(x)) x floor(sqrt(x))
+          auto correct_result = std::make_pair<double, double>(
+                ceil(sqrt(i)) * objects.front()->width(),
+                floor(sqrt(i)) *  objects.front()->width());
+          // make sure the bin is the correct size
+          CHECK_EQ(correct_result, p.pack());
+          // arrange cubes
+          p.arrange(0,0);
+          // check location of each moved cube
         }
-        auto p = sse::Packer(objects);
-
-        // the bin dimensions of a list of cubes (X elements long) will always be
-        // ceil(sqrt(x)) x floor(sqrt(x))
-        auto correct_result = std::make_pair<double, double>(
-            ceil(sqrt(i)) * objects.front()->width(),
-            floor(sqrt(i)) *  objects.front()->width());
-        // make sure the bin is the correct size
-        CHECK_EQ(correct_result, p.pack());
       }
     }
   }
 }
+
