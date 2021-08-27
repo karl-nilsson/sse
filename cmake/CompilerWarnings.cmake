3a8a5ab7737129b28bb8f02a042bfd89f3407f33
Karl Nilsson <karl.robert.nilsson@gmail.com>
2021-08-27 19:33:02 -0400

    meta: tweak codespell workflow, spelling fixes

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -67,7 +67,7 @@ function(set_project_warnings project_name)
 
   set(GCC_WARNINGS
       ${CLANG_WARNINGS}
-      -Wmisleading-indentation # warn if identation implies blocks where blocks
+      -Wmisleading-indentation # warn if indentation implies blocks where blocks
                                # do not exist
       -Wduplicated-cond # warn if if / else chain has duplicated conditions
       -Wduplicated-branches # warn if if / else branches have duplicated code
